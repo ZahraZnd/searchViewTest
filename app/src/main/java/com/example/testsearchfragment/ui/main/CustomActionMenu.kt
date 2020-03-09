2ac9596d7053391b8772b1590a226c985ea3be7e
Zahra Z <zahrazandieh@gmail.com>
2020-03-09 20:56:58 +0330

    push action menu

--- app/src/main/java/com/example/testsearchfragment/ui/main/CustomActionMenu.kt
+++ app/src/main/java/com/example/testsearchfragment/ui/main/CustomActionMenu.kt
@@ -1,5 +1,6 @@
 package com.example.testsearchfragment.ui.main
 
+import android.annotation.SuppressLint
 import android.graphics.Typeface
 import android.text.style.StyleSpan
 import android.text.SpannableStringBuilder
@@ -22,50 +23,55 @@ class CustomActionMenu(bV: TextView) : ActionMode.Callback,
 
     override fun onCreateActionMode(mode: android.view.ActionMode?, menu: Menu?): Boolean {
         Log.d(TAG, "onCreateActionMode")
+//        menu?.clear()
         val inflater = mode?.menuInflater
         inflater?.inflate(R.menu.textclick, menu)
-        menu?.removeItem(android.R.id.selectAll)
         return true
     }
 
     override fun onPrepareActionMode(p0: android.view.ActionMode?, menu: Menu?): Boolean {
-        menu?.removeItem(android.R.id.selectAll)
-        // Remove the "cut" option
-        menu?.removeItem(android.R.id.cut)
-        // Remove the "copy all" option
-        menu?.removeItem(android.R.id.copy) 
-        // Remove the "dictionary" option
-        menu?.clear()
+//        menu?.removeItem(android.R.id.selectAll)
+//        // Remove the "cut" option
+//        menu?.removeItem(android.R.id.cut)
+//        // Remove the "copy all" option
+//        menu?.removeItem(android.R.id.copy)
+//        // Remove the "dictionary" option
+
         menu?.add(R.id.dictinory)
         menu?.add(R.id.terminology)
         // Remove the "shared" option
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            menu?.removeItem(android.R.id.shareText)
-        }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            menu?.removeItem(android.R.id.shareText)
+//        }
         return false
     }
 
-    override fun onActionItemClicked(p0: android.view.ActionMode?, item: MenuItem?): Boolean {
-        Log.d(
-            TAG,
-            String.format("onActionItemClicked item=%s/%d", item.toString(), item?.itemId)
-        )
-        val cs: CharacterStyle
-        val start = bodyView.selectionStart
-        val end = bodyView.selectionEnd
-        val ssb = SpannableStringBuilder(bodyView.text)
+//    override fun onActionItemClicked(p0: android.view.ActionMode?, item: MenuItem?): Boolean {
+//        Log.d(
+//            TAG,
+//            String.format("onActionItemClicked item=%s/%d", item.toString(), item?.itemId)
+//        )
+//        val cs: CharacterStyle
+//        val start = bodyView.selectionStart
+//        val end = bodyView.selectionEnd
+//        val ssb = SpannableStringBuilder(bodyView.text)
+//
+//        when (item?.itemId) {
+//
+//            R.id.italic -> {
+//                cs = StyleSpan(Typeface.ITALIC)
+//                ssb.setSpan(cs, start, end, 1)
+//                bodyView.text = ssb
+//                return true
+//            }
+//
+//        }
+//        return false
+//    }
 
-        when (item?.itemId) {
-
-            R.id.italic -> {
-                cs = StyleSpan(Typeface.ITALIC)
-                ssb.setSpan(cs, start, end, 1)
-                bodyView.text = ssb
-                return true
-            }
-
-        }
-        return false
+    override fun onActionItemClicked (p0: android.view.ActionMode?, item: MenuItem?): Boolean {
+        Log.d("IDZAHRA","${item?.itemId}")
+        return true
     }
 
     override fun onDestroyActionMode(p0: android.view.ActionMode?) = Unit
